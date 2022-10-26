157db7b1e0d9eb9e016d46d0225798e42f95ffbd
watitwa ivy <watitwaivy@gmail.com>
2022-10-26 09:46:41 +0300

    models

--- musicapp/models.py
+++ musicapp/models.py
@@ -2,26 +2,27 @@ from django.db import models
 from datetime import datetime
 
 class Artiste(models.Model):
-    Song = models.OneToOneField("Song", on_delete =models.CASCADE)
+   
     first_name = models.CharField(max_length=400)
     last_name = models.CharField(max_length=400)
-    age = models.IntegerField()
+    age = models.IntegerField(default=1)
+    song=models.ForeignKey('Song', null=True, blank=True,on_delete =models.CASCADE )
     def __str__(self):
-        return self.first_name
+        return f'{self.first_name}{self.last_name}'
 class Song(models.Model):
-        Lyric = models.OneToOneField("Lyric", on_delete=models.CASCADE)
-        Artiste = models.ForeignKey(Artiste, on_delete =models.CASCADE)
-        title = models.CharField(max_length=400)
-        date_released = models.DateField(default = datetime.today)
-        likes =models.IntegerField()
-        
-        def __str__(self):
-            return self.title
+    artiste_id=models.CharField(max_length=20)
+    title = models.CharField(max_length=400)
+    date_released = models.DateTimeField(auto_now_add=True)
+    likes =models.PositiveIntegerField(default=0)
+    lyric = models.ForeignKey('Lyric', null=True, blank=True,on_delete =models.SET_NULL )
+                                
+    def __str__(self):
+        return self.title                         
 class Lyric(models.Model):
-        Song = models.ForeignKey(Song, on_delete=models.CASCADE)
-        content =models.CharField(max_length=400)
+    song_id = models.CharField(max_length=20)
+    content =models.CharField(max_length=400)
         
-        def __str__(self):
+    def __str__(self):
             return self.content
 
 
